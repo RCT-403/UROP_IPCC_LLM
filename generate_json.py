3645d59736e026f0f7d48960dd4e0533ed8cef4c
Ronic725 <91591480+Ronic725@users.noreply.github.com>
2025-03-11 11:04:27 +0800

    hot fix on decomposition of ipcc

--- generate_json.py
+++ generate_json.py
@@ -4,15 +4,16 @@ import json
 from decompo_ipcc import build_report
 from finetuning.src.preprocessing.format_data import format_data, create_training_examples
 
-pdf_path = "./data/raw/IPCC AR6 Chapter 2 Climate System.pdf"
+chapter = 11
+pdf_path = f"./data/raw/IPCC AR6 Chapter {chapter}.pdf"
 report = build_report(pdf_path)
 formatted_data = format_data(report.to_dict())
 training_examples = create_training_examples(formatted_data)
 
-output_path = "./data/processed/formatted_data.json"
+output_path = f"./data/processed/formatted_data_{chapter}.json"
 with open(output_path, 'w') as json_file:
     json.dump(formatted_data, json_file, indent=4)
 
-output_path = "./data/processed/training_examples.json"
+output_path = f"./data/processed/training_examples_{chapter}.json"
 with open(output_path, 'w') as json_file:
     json.dump(training_examples, json_file, indent=4)
